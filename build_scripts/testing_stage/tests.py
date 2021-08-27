bf7d1fce3857612e16116c4107149a521bafaf17
homeperson3 <a10@live.ca>
2021-08-27 18:47:31 -0400

    Fix test floating point inaccuracies

--- build_scripts/testing_stage/tests.py
+++ build_scripts/testing_stage/tests.py
@@ -19,7 +19,16 @@ def setTransform():
     return x
 
 def test_collisions():
-    assert collisions() == [0.5000028014183044, 0.4999975562095642, 0.49999725818634033, 0.500002384185791, 0.0002511556085664779, 1.9999995231628418, 0.0012792845955118537]
+    result = collisions()
+    assert ((result[0] - 0.5) < 0.001)
+    assert ((result[1] - 0.5) < 0.001)
+    assert ((result[2] - 0.5) < 0.001)
+    assert ((result[3] - 0.5) < 0.001)
+    assert ((result[4] - 0) < 0.001)
+    assert ((result[5] - 2) < 0.001)
+    assert ((result[6] - 0.001) < 0.001)
+    #assert #1 == 0
+    #assert #collisions() == [0.5000028014183044, 0.4999975562095642, 0.49999725818634033, 0.500002384185791, 0.0002511556085664779, 1.9999995231628418, 0.0012792845955118537]
 
 def test_setTransform():
     assert setTransform() == [0.5,0.5,0.5,0.5,3.0,1.0,3.0]
